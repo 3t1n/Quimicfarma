87170acb1ef3416856c0c146acd3d7cd289356b4
reynaldo <reynaldotadeu10@gmail.com>
2019-03-25 22:05:26 -0300

    ultima

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,60 +1,61 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>QuimicFarma</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat%7CRoboto:300,400,700" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>QuimicFarma</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/algolia.css') }}">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat%7CRoboto:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    </head>
-    <body>
-        <div id="app">
-            <header>
-                <div class="top-nav container">
-                    <div class="top-nav-left">
-                        <div class="logo">QuimicFarma</div>
-                        <div class="aa-input-container" id="aa-input-container">
-    <input type="search" id="aa-search-input" class="aa-input-search" placeholder="Procurando algo?" name="search"
-        autocomplete="off" />
-    <svg class="aa-input-icon" viewBox="654 -372 1664 1664">
-        <path d="M1806,332c0-123.3-43.8-228.8-131.5-316.5C1586.8-72.2,1481.3-116,1358-116s-228.8,43.8-316.5,131.5  C953.8,103.2,910,208.7,910,332s43.8,228.8,131.5,316.5C1129.2,736.2,1234.7,780,1358,780s228.8-43.8,316.5-131.5  C1762.2,560.8,1806,455.3,1806,332z M2318,1164c0,34.7-12.7,64.7-38,90s-55.3,38-90,38c-36,0-66-12.7-90-38l-343-342  c-119.3,82.7-252.3,124-399,124c-95.3,0-186.5-18.5-273.5-55.5s-162-87-225-150s-113-138-150-225S654,427.3,654,332  s18.5-186.5,55.5-273.5s87-162,150-225s138-113,225-150S1262.7-372,1358-372s186.5,18.5,273.5,55.5s162,87,225,150s113,138,150,225  S2062,236.7,2062,332c0,146.7-41.3,279.7-124,399l343,343C2305.7,1098.7,2318,1128.7,2318,1164z" />
-    </svg>
-</div>
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/algolia.css') }}">
+
+</head>
+
+<body>
+    <div id="app">
+        <header>
+            <div class="top-nav container">
+                <div class="top-nav-left">
+                    <div class="logo">QuimicFarma</div>
+                    <div class="aa-input-container" id="aa-input-container">
+                        <input type="search" id="aa-search-input" class="aa-input-search" placeholder="Procurando algo?" name="search" autocomplete="off" />
+                        <svg class="aa-input-icon" viewBox="654 -372 1664 1664">
+                            <path d="M1806,332c0-123.3-43.8-228.8-131.5-316.5C1586.8-72.2,1481.3-116,1358-116s-228.8,43.8-316.5,131.5  C953.8,103.2,910,208.7,910,332s43.8,228.8,131.5,316.5C1129.2,736.2,1234.7,780,1358,780s228.8-43.8,316.5-131.5  C1762.2,560.8,1806,455.3,1806,332z M2318,1164c0,34.7-12.7,64.7-38,90s-55.3,38-90,38c-36,0-66-12.7-90-38l-343-342  c-119.3,82.7-252.3,124-399,124c-95.3,0-186.5-18.5-273.5-55.5s-162-87-225-150s-113-138-150-225S654,427.3,654,332  s18.5-186.5,55.5-273.5s87-162,150-225s138-113,225-150S1262.7-372,1358-372s186.5,18.5,273.5,55.5s162,87,225,150s113,138,150,225  S2062,236.7,2062,332c0,146.7-41.3,279.7-124,399l343,343C2305.7,1098.7,2318,1128.7,2318,1164z" />
+                        </svg>
                     </div>
-                    <div class="top-nav-right">
+                </div>
+                <div class="top-nav-right">
                     @include('partials.menus.main-right')
-                    </div>
+                </div>
 
-                    </div>
-                </div> <!-- end top-nav -->
-            </header>
-            <div class="nav-baixo">
-            down
+            </div>
+    </div> <!-- end top-nav -->
+    </header>
+    <div class="nav-baixo">
+        down
+    </div>
+
+    <div class="featured-section">
+
+        <div class="container">
+            <h1 class="text-center">QuimicFarma</h1>
+
+            <p class="section-description">muda dps</p>
+
+            <div class="text-center button-container">
+                <a href="#" class="button">Featured</a>
+                <a href="#" class="button">On Sale</a>
             </div>
 
-            <div class="featured-section">
-
-                <div class="container">
-                    <h1 class="text-center">QuimicFarma</h1>
-
-                    <p class="section-description">muda dps</p>
-
-                    <div class="text-center button-container">
-                        <a href="#" class="button">Featured</a>
-                        <a href="#" class="button">On Sale</a>
-                    </div>
-
-                    {{-- <div class="tabs">
+            {{-- <div class="tabs">
                         <div class="tab">
                             Featured
                         </div>
@@ -63,38 +64,41 @@
                         </div>
                     </div> --}}
 
-                    <div class="products text-center">
-                        @foreach ($products as $product)
-                            <div class="product">
-                                <a href="{{ route('shop.show', $product->slug) }}"><img src="{{ productImage($product->image) }}" alt="product"></a>
-                                <a href="{{ route('shop.show', $product->slug) }}"><div class="product-name">{{ $product->name }}</div></a>
-                                <div class="product-price">{{ $product->presentPrice() }}</div>
-                            </div>
-                        @endforeach
+            <div class="products text-center">
+                @foreach ($products as $product)
+                <div class="product">
+                    <a href="{{ route('shop.show', $product->slug) }}"><img src="{{ productImage($product->image) }}" alt="product"></a>
+                    <a href="{{ route('shop.show', $product->slug) }}">
+                        <div class="product-name">{{ $product->name }}</div>
+                    </a>
+                    <div class="product-price">{{ $product->presentPrice() }}</div>
+                </div>
+                @endforeach
 
-                    </div> <!-- end products -->
+            </div> <!-- end products -->
 
-                    <div class="text-center button-container">
-                        <a href="{{ route('shop.index') }}" class="button">Veja mais produtos</a>
-                    </div>
+            <div class="text-center button-container">
+                <a href="{{ route('shop.index') }}" class="button">Veja mais produtos</a>
+            </div>
 
-                </div> <!-- end container -->
+        </div> <!-- end container -->
 
-            </div> <!-- end featured-section -->
-
-
+    </div> <!-- end featured-section -->
 
 
-            @include('partials.footer')
 
-        </div> <!-- end #app -->
-        <script src="js/app.js"></script>
-    
+
+    @include('partials.footer')
+
+    </div> <!-- end #app -->
+    <script src="js/app.js"></script>
+
     <!-- Include AlgoliaSearch JS Client and autocomplete.js library -->
     <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
     <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
     <script src="{{ asset('js/algolia.js') }}"></script>
 
 
-    </body>
-</html>
+</body>
+
+</html> 
